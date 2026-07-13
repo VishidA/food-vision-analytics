a64d183ddbf22f24a80e42561f33c66d8b9cfd8f
Oleksandr <sasha220607@gmail.com>
2026-07-13 16:38:19 +0300

    add Users Table

---
+++ src/db/models/users.py
@@ -0,0 +1,17 @@
+from base import Base
+import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import VARCHAR, DateTime
+
+class Users(Base):
+    __tablename__ = "Users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    login_name: Mapped[str] = mapped_column(VARCHAR(30), nullable=False)
+    hash_password: Mapped[str] = mapped_column(VARCHAR(150), nullable=False)
+    name: Mapped[str] = mapped_column(VARCHAR(15), nullable=False)
+    register_date: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), 
+        default=datetime.datetime.utcnow,
+        nullable=False
+    )
