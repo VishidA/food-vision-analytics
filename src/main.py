d46fdf23d77ff322e792b5882be6127cdde0afb1
Oleksandr <sasha220607@gmail.com>
2026-07-14 12:37:46 +0300

    fix main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
 from db.database import engine
 from db.models.base import Base
+from db.seed_spoonacular import seed_database_from_spoonacular
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def on_startup() -> None:
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- Startup ---
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    
+    await seed_database_from_spoonacular()
+    yield
+    
+
+app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
